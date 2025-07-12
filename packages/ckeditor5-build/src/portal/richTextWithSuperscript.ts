b758764aa37de743619c40e3c0dec64445218420
Kirk Santos <kirk.santos@sanofi.com>
2025-07-12 10:48:56 +0800

    feat: RichTextWithSuperscriptEditor

---
+++ packages/ckeditor5-build/src/portal/richTextWithSuperscript.ts
@@ -0,0 +1,51 @@
+import { ClassicEditor as CkEditor5ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import type { EditorConfig } from 'ckeditor5/src/core';
+
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { Superscript } from '@ckeditor/ckeditor5-basic-styles';
+import { Table, TableToolbar, TableColumnResize, TableUtils, TableProperties, TableCellProperties } from '@ckeditor/ckeditor5-table';
+
+import './../mgnl-css-framework-compatible.css';
+
+const BUILTIN_PLUGINS = [
+	Essentials,
+	Heading,
+	Superscript,
+	Table,
+	TableToolbar,
+	TableColumnResize,
+	TableUtils,
+	TableProperties,
+	TableCellProperties,
+];
+
+const DEFAULT_CONFIG: EditorConfig = {
+	toolbar: [
+		'superscript',
+		'|',
+		'heading',
+		'|',
+		'insertTable'
+	],
+	heading: {
+		options: [
+			{ model: 'headingLegal', view: { name: 'p', classes: 'legal-text' }, title: 'Legal', class: 'ck-heading_legal' },
+			{ model: 'headingSmall', view: { name: 'p', classes: 'small-text' }, title: 'Small', class: 'ck-heading_small' }
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells',
+			'tableCellProperties',
+			'tableProperties'
+		]
+	},
+};
+
+export class RichTextWithSuperscriptEditor extends CkEditor5ClassicEditor {
+	public static override builtinPlugins = BUILTIN_PLUGINS;
+	public static override defaultConfig = DEFAULT_CONFIG;
+}
